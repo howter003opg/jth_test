50991554654a9af6595d494e1a9b4eb72c5b6f4a
stc_179-PC\stc_179 <howter003opg@naver.com>
2018-05-02 16:49:15 +0900

    LED_SHIFT

--- AVR_led/main.c
+++ AVR_led/main.c
@@ -16,8 +16,17 @@ int main(void)
 	
     while (1) 
     {
-		PORTD = 0xff;
+		for (int i =0; i<7; i++)
+		{
+		PORTD = ~(0x01<< i);
 		_delay_ms(300);
+		}
+		for (int i =0; i<7; i++)
+		{
+			PORTD = ~(0x80>> i);
+			_delay_ms(300);
+		}
+		
 		PORTD = 0x00;
 		_delay_ms(300);
     }
